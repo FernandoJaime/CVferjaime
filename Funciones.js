496d5d2a51a2824ddd56744a0b14d1ede1f50567
Fer Jaime <Fernando.Jaime@alumnos.uai.edu.ar>
2023-02-27 15:55:13 -0300

    inicio de la pagina web

---
+++ Funciones.js
@@ -0,0 +1,19 @@
+/* Funcion que al presionar uno de los iconos te redirecciona a su link correspondiente */
+function redirectToLink(event) {
+    const link = event.currentTarget.getAttribute('href');
+    window.location.href = link;
+  }
+  const icons = document.querySelectorAll('.iconos i');
+  icons.forEach(icon => icon.addEventListener('click', redirectToLink));
+
+/* Funcion para mostrar información de la pagina */
+const boton = document.querySelector('.button');
+const informacion = document.querySelector('#info');
+
+boton.addEventListener('click', () => {
+  if (informacion.style.display === 'none') {
+    informacion.style.display = 'block';
+  } else {
+    informacion.style.display = 'none';
+  }
+});
